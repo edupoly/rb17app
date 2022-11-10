810057fa9421554029499b00ca8f57ddc98bb805
edupoly <info.edupoly@gmail.com>
2022-11-10 08:40:28 +0530

    bootstrapped master

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container border border-info p-1">
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
