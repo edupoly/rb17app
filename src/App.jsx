0a40cc0afa41e1fcc0bac79fc2d03406431d5bc4
edupoly <info.edupoly@gmail.com>
2022-11-18 08:55:22 +0530

    class component with counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="container border border-info p-1">
-      <h1>Welcome</h1>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      count:0
+    }
+  }
+  inc=()=>{
+    this.setState({count:this.state.count+1})
+  }
+  dec=()=>{
+    this.setState({count:this.state.count-1})
+  }
+  render(){
+    return(
+      <div className='border border-danger border-2 m-2 p-2'>
+        <h1>Lets understand Class Components</h1>
+        <h1>Count:{this.state.count}</h1>
+        <button onClick={this.inc}>Increment</button>
+        <button onClick={this.dec}>Decrement</button>
+      </div>
+    )
+  }
 }
 
 export default App;
